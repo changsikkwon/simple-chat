33792f0b148eb3e464858570621b1e538427dc41
chang sik kwon <kcs15987@gmail.com>
2021-04-25 21:11:24 +0900

    chat

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url('chat/<room_name>', consumers.ChatConsumer),
+    url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
 ]
